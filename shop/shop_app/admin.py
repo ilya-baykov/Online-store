153177080a4cd2234321a235c4748db272bbce86
ilya-baykov <i-baykov@internet.ru>
2024-01-16 19:15:23 +0300

    Создал админ панель для отображения категорий и продуктов

--- shop/shop_app/admin.py
+++ shop/shop_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'name']
+    list_display_links = ['slug', 'name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'update',
+                    'image', 'description']
+    list_filter = ['available', 'price', 'update']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
