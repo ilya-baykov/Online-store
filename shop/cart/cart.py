26fc051815a31abf5287e146902508f4f6746d94
ilya-baykov <i-baykov@internet.ru>
2024-01-17 17:40:40 +0300

    Добавил __iter__  в классе Cart

--- shop/cart/cart.py
+++ shop/cart/cart.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
-from ..shop_app import models
+from ..shop_app.models import *
 
 
 class Cart:
@@ -39,3 +39,21 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        """ Прокрутить товарные позиции корзины в цикле
+            И получить товары из базы данных
+        """
+        product_ids = self.cart.keys()
+
+        # Получить объекты product и добавить их в корзину
+
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
