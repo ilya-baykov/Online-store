39ae4942edc3eed727ba51295ade4ecb5e5a8ad7
ilya-baykov <i-baykov@internet.ru>
2024-01-17 17:42:05 +0300

    Добавил __len__  в классе Cart

--- shop/cart/cart.py
+++ shop/cart/cart.py
@@ -57,3 +57,9 @@ class Cart:
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
             yield item
+
+    def __len__(self):
+        """
+        Посчитать все товарные позиции в корзине
+        """
+        return sum(item['quantity'] for item in self.cart.values())
