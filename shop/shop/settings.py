dc1126967b3252c3034aae4c747e49914008633b
ilya-baykov <i-baykov@internet.ru>
2024-01-17 17:12:18 +0300

    Начал реализаю корзины Создал отдельное приложение Cart Начал разработку класса корзины Добавил методы save , add, remove

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop_app.apps.ShopAppConfig',
+    'cart.apps.CartConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+CART_SESSION_ID = 'cart'
